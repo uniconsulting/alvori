e7d7c21b06e870dc335059569fd34f2b5e1466a2
uni.kit <uni.ai.consalting@gmail.com>
2026-03-26 20:55:16 +0400

    Update site-path.ts

--- src/lib/site-path.ts
+++ src/lib/site-path.ts
@@ -1,6 +1,1 @@
-export const sitePath =
-  process.env.NODE_ENV === 'production' && process.env.GITHUB_REPOSITORY
-    ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}`
-    : '';
-
 export const sitePath = process.env.NEXT_PUBLIC_BASE_PATH ?? '';
