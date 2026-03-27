fb5ba4cc196636f8305fd0cbe11a69eb75c39d37
uni.kit <uni.ai.consalting@gmail.com>
2026-03-27 19:30:56 +0400

    Update navigation.ts

--- src/content/navigation.ts
+++ src/content/navigation.ts
@@ -2,5 +2,6 @@ export const navigation = [
   { label: 'услуги', href: '#services' },
   { label: 'стоимость', href: '#pricing' },
   { label: 'о компании', href: '#about' },
+  { label: 'география', href: '#geography' },
   { label: 'контакты', href: '#contacts' },
-];
+] as const;
