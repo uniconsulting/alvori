32ab588d8fd9b3a0746c4914844e68680e2739f8
uni.kit <uni.ai.consalting@gmail.com>
2026-03-30 20:16:16 +0400

    Update AutoParkSection.tsx

--- src/components/sections/AutoParkSection.tsx
+++ src/components/sections/AutoParkSection.tsx
@@ -33,7 +33,7 @@ export function AutoParkSection() {
               '--top-h': '78px',
               '--section-gap': '20px',
               '--truck-title-w': '314px',
-              '--trailer-title-w': '318px',
+              '--trailer-title-w': '320px',
             } as React.CSSProperties
           }
         >
